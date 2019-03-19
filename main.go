fd57326e67ad1f69e20eeac949c0a3a410873783
Fernando <a11199@alunos.ipca.pt>
2019-03-19 13:56:15 +0000

    Concurrency example using Go

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+func main() {
+	c := make(chan string)
+	go count("Nando", c)
+
+	for msg := range c {
+		fmt.Println(msg)
+	}
+}
+
+func count(thing string, c chan string) {
+	for i := 1; i <= 5; i++ {
+		c <- thing
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	close(c)
+}*/
+
+func main() {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+	go worker(jobs, results)
+
+	for i := 0; i < 100; i++ {
+		jobs <- 1
+	}
+
+	close(jobs)
+
+	for j := 0; j < 100; j++ {
+		fmt.Println(<-results)
+	}
+}
+
+func worker(jobs <-chan int, results chan<- int) {
+	for n := range jobs {
+		results <- fib(n)
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
+}
